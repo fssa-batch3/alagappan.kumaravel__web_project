96f742982e4b1b527763a3eb333001f370bf8e3e
alagappan2578 <116268706+alagappan2578@users.noreply.github.com>
2023-02-16 13:40:35 +0530

    worked on time and range value

--- assets/css/login.js
+++ assets/css/login.js
@@ -1,38 +1,53 @@
 function signUp_1(e) {
+    e.preventDefault();
     // hear i collect value from signUp form 
     let phonenumber = document.getElementById("phonenumber").value,	
-	fullname = document.getElementById("fullname").value,
 	 username = document.getElementById("username").value,
-	 password = document.getElementById("password").value;
-
+	 password = document.getElementById("password").value,
+     confrim_password = document.getElementById("confirm_password").value;
 
      // hear i give var name for local storage data (initially there is no data so we mentioned or (||) symbol to get empty array)
     let user_detail = JSON.parse(localStorage.getItem('user_detail')) || [];
 
     // hear we give some condition for signup to restict same unique id 
-    let exist = user_detail.some(data =>
-            data.userName == username ||
-            data.phoneNumber == phonenumber
-    );
 
-    // if condition fail
-    if(!exist){
-        user_detail_single = { "phoneNumber": phonenumber, "fullName" :fullname, "userName" :username, "password" : password}
+    let same_number = user_detail.some(data =>
+        data.phoneNumber == phonenumber );
+
+    if(same_number){
+        // alert("This number already have account");
+       a = document.querySelector(".wrong_password").innerHTML = "This number already have account.  ";
+        // document.querySelector('form').reset();
+        return a;
+    }
+    let same_user_name = user_detail.some(data =>
+        data.userName == username );
+            
+    if(same_user_name){
+        // alert("User name not available.");
+        a = document.querySelector(".wrong_password").innerHTML = "User name not available.  "
+        return a;
+    }
+
+    let wrong_password = password != confrim_password ;
+    
+    if (wrong_password){
+        // alert("Password not match.");
+        
+        a = document.querySelector(".wrong_password").innerHTML = "Password not match.  "
+        return a;
+    }
+    else {
+        user_detail_single = { "phoneNumber": phonenumber, "userName" :username, "password" : password , "confirmPassword" : confrim_password}
 
         localStorage.setItem('user_data', JSON.stringify(user_detail_single))   
         
         document.querySelector('form').reset();
 
-        alert('Account created Successfully');
-
     	location.href = "./signup2.html";
     }
-    // if condition pass
-    else {
-        alert('Sorry the User already Exist!! \n Try with different Username or phonenumber');
-    }
     
-    e.preventDefault();
+    
 }
 
 function signUp_2(e) {
@@ -41,7 +56,11 @@ function signUp_2(e) {
     let date_of_birth = document.getElementById("date_of_birth").value,	
 	 gender = document.getElementById("gender").value,
 	 game = document.getElementById("game").value,
-	 area = document.getElementById("area").value;
+	 area = document.getElementById("area").value,
+     distric = document.getElementById("distric").value,
+     create_date = moment();
+     console.log(create_date)
+
 
 
     let user_data = JSON.parse(localStorage.getItem("user_data"));
@@ -50,6 +69,11 @@ function signUp_2(e) {
     user_data["gender"] = gender;
     user_data["game"] = game;
     user_data["area"] = area;
+    user_data["distric"] = distric;
+    user_data["createDate"] = create_date;
+    user_data["firstName"] = "";
+    user_data["lastName"] = "";
+    user_data["about"]= "";
 
     // hear i give var name for local storage data (initially there is no data so we mentioned or (||) symbol to get empty array)
     let user_detail = JSON.parse(localStorage.getItem('user_detail')) || [];
@@ -59,8 +83,6 @@ function signUp_2(e) {
         localStorage.setItem('user_detail', JSON.stringify(user_detail));
         
         document.querySelector('form').reset();
-
-        alert('Account created Successfully');
 
     	window.location.href = "../homepage/hpnew.html"
 
@@ -81,7 +103,7 @@ function signIn(e){
      && data.password == password);
 
     if(!exist){
-        alert("Incorrect login credentials");
+        document.querySelector("#error_msg").innerHTML = "Wrong Password or Phone number"
     }
     else{
         user_detail_single = { "phoneNumber": phonenumber}
@@ -93,4 +115,110 @@ function signIn(e){
 
     }
     e.preventDefault();
+}
+
+// function for profile edit.
+
+function onEdit(){
+    window.location.href = "./profileedit.html";
+
+}
+
+
+
+function update(e){
+    e.preventDefault();
+
+let user_data = JSON.parse(localStorage.getItem('user_data'));
+
+let unique_id = user_data["phoneNumber"];
+
+let user_detail = JSON.parse(localStorage.getItem('user_detail'))
+
+
+function findPlayer(a) {
+return a.phoneNumber == unique_id ;
+}
+
+person_data = user_detail.find(findPlayer);
+
+// for unique user name start
+person_data["userName"] = "";
+
+let user_name = document.getElementById("user_username").value;
+
+let same_user_name = user_detail.some(data =>
+    data.userName == user_name );
+if(same_user_name){
+    b = document.querySelector(".wrong_password").innerHTML = "User name not available.  "
+    return b;
+}
+// for unique user name end
+
+let first_name = document.getElementById("user_first_name").value,
+last_name = document.getElementById("user_last_name").value,
+date_of_birth = document.getElementById("user_date_of_birth").value ,
+gender = document.getElementById("user_gender").value ,
+game = document.getElementById("user_game").value ,
+area = document.getElementById("user_area").value ,
+distric = document.getElementById("user_distric").value ,
+about = document.getElementById("user_about").value ,
+image = document.getElementById("user_image").value;
+
+
+person_data["userName"] = user_name;
+person_data["firstName"] = first_name;
+person_data["lastName"] = last_name;
+person_data["dateOFBirth"] = date_of_birth;
+person_data["gender"] = gender;
+person_data["game"] = game;
+person_data["area"] = area;
+person_data["distric"] = distric;
+person_data["about"]= about;
+person_data["imageUrl"]= image;
+
+localStorage.setItem('user_detail', JSON.stringify(user_detail));
+
+window.location.href = "./myprofile.html";
+
+}
+
+// delete profile start
+
+function del(){
+    
+    if(confirm("Are you sure to delete this Account ?")){
+let user_data = JSON.parse(localStorage.getItem('user_data'));
+
+let unique_id = user_data["phoneNumber"];
+
+let user_detail = JSON.parse(localStorage.getItem('user_detail'))
+
+
+function findPlayer(a) {
+return a.phoneNumber == unique_id ;
+}
+
+let person_data = user_detail.find(findPlayer);
+
+let indexOfUser = user_detail.indexOf(person_data);
+
+user_detail.splice(indexOfUser, 1);
+
+
+localStorage.setItem('user_detail', JSON.stringify(user_detail));
+
+localStorage.setItem('user_data', "")
+
+window.location.href = "../../index.html";
+
+    }
+
+}
+
+function logOut(){
+    localStorage.setItem('user_data', "")
+    window.location.href = "../../index.html";
+
+
 }
